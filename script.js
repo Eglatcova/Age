78ba028a731a9146abc06776606c8ee86fe93a1f
Eglatcova <e.glatcova@yandex.ru>
2020-05-24 17:48:58 +0300

    with class

--- script.js
+++ script.js
@@ -6,27 +6,54 @@ const daySelect = document.querySelector("#day"),
   button = document.querySelector(".button"),
   result = document.querySelector(".result");
 
-let countAge = () => {
-  let nowDate = new Date(),
-    userYear = yearSelect.value,
-    userMonth = monthSelect.value,
-    userDay = daySelect.value,
-    birthdayDate = new Date(userYear, userMonth, userDay),
-    nowYear = new Date().getFullYear(),
-    nowUseYear = birthdayDate.getFullYear(),
-    ageYear = nowYear - nowUseYear;
+class userAge {
+  constructor() {}
 
-  if (nowDate.getMonth() + 1 < birthdayDate.getMonth()) {
-    ageYear = ageYear - 1;
-  } else {
+  static countAge(userYear, userMonth, userDay) {
+    let nowDate = new Date(),
+      birthdayDate = new Date(userYear, userMonth, userDay),
+      nowYear = new Date().getFullYear(),
+      nowUseYear = birthdayDate.getFullYear(),
+      ageYear = nowYear - nowUseYear;
     if (
-      nowDate.getMonth() + 1 === birthdayDate.getMonth() &&
-      nowDate.getDate() < birthdayDate.getDate()
+      nowDate.getMonth() + 1 < birthdayDate.getMonth() ||
+      (nowDate.getMonth() + 1 === birthdayDate.getMonth() &&
+        nowDate.getDate() < birthdayDate.getDate())
     ) {
       ageYear = ageYear - 1;
     }
+    return ageYear;
   }
-  result.textContent = ageYear;
+
+  static ageWord(age) {
+    let lastNumber = age % 10;
+    if (age > 10 && age < 20) {
+      return "лет";
+    }
+    switch (lastNumber) {
+      case 0:
+        return "лет";
+      case 1:
+        return "год";
+      case 2:
+      case 3:
+      case 4:
+        return "года";
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+        return "лет";
+    }
+  }
+}
+
+const showAge = () => {
+  result.textContent =
+    userAge.countAge(1998, 5, 23) +
+    " " +
+    userAge.ageWord(userAge.countAge(1998, 5, 23));
 };
 
-button.addEventListener("click", countAge);
+button.addEventListener("click", showAge);
